3f379cf0d18edd5142d1843b02c6420e04d7c80d
root <root@alog.com>
2019-09-24 18:02:09 +0800

    提交

---
+++ src/main/java/org/aztec/spring/demo/config/client/Application.java
@@ -0,0 +1,29 @@
+package org.aztec.spring.demo.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ServletComponentScan
+@RestController
+public class Application {
+	
+	  @Value("${democonfigclient.message}")
+	  String name = "World";
+
+	  @RequestMapping("/")
+	  public String home() {
+	    return "Hello " + name;
+	  }
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
