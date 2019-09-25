adff8437356a0c6d7c5c4ce7f9ba54f4988ee753
root <root@alog.com>
2019-09-25 18:01:38 +0800

    commit

--- spring-cloud-config-server/src/main/java/org/aztec/spring/cloud/config/server/ConfigServer.java
+++ spring-cloud-config-server/src/main/java/org/aztec/spring/cloud/config/server/ConfigServer.java
@@ -5,21 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
 @EnableEurekaServer
-//@RestController
+@RestController
 public class ConfigServer {
 
-  //@Value("${config.name}")
-  String name = "World";
-
-  //@RequestMapping("/")
-  public String home() {
-    return "Hello " + name;
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(ConfigServer.class, args);
--- src/main/java/org/aztec/spring/demo/config/client/Application.java
+++ src/main/java/org/aztec/spring/demo/config/client/Application.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableEurekaClient
 @RestController
 public class Application {
 	
